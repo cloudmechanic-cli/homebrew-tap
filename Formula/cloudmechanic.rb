60d818e7258daa8915b716240357e537dfc3cc6d
code wizard <codewizard@gmail.com>
2026-04-14 05:15:10 +0200

    cloudmechanic 1.6.1 — fix --all-regions without AWS_REGION

--- Formula/cloudmechanic.rb
+++ Formula/cloudmechanic.rb
@@ -1,28 +1,28 @@
 class Cloudmechanic < Formula
   desc "An OBD scanner for your AWS environment. Find cost leaks and security vulnerabilities in seconds."
   homepage "https://github.com/cloudmechanic-cli/cloudmechanic"
-  version "1.6.0"
+  version "1.6.1"
   license "MIT"
 
   on_macos do
     on_arm do
-      url "https://github.com/cloudmechanic-cli/cloudmechanic/releases/download/v1.6.0/cloudmechanic_1.6.0_darwin_arm64.tar.gz"
-      sha256 "fa8aa5047cedfdb3b772e1685c243e465140f1063575bf749c453cc0899a1b6b"
+      url "https://github.com/cloudmechanic-cli/cloudmechanic/releases/download/v1.6.1/cloudmechanic_1.6.1_darwin_arm64.tar.gz"
+      sha256 "fe8051d0cbcfb50d14a6077a187db3823da26d0e10a4e558531dea16741e9544"
     end
     on_intel do
-      url "https://github.com/cloudmechanic-cli/cloudmechanic/releases/download/v1.6.0/cloudmechanic_1.6.0_darwin_amd64.tar.gz"
-      sha256 "18dd93c1d7c87611a09bd76b7c9a082949ba90f0b5e856831d026f386c9010e7"
+      url "https://github.com/cloudmechanic-cli/cloudmechanic/releases/download/v1.6.1/cloudmechanic_1.6.1_darwin_amd64.tar.gz"
+      sha256 "ce6e7063b5c54cb0ffd478a153c0956d1921d806a3fba79a8c3dfd0b8695e421"
     end
   end
 
   on_linux do
     on_arm do
-      url "https://github.com/cloudmechanic-cli/cloudmechanic/releases/download/v1.6.0/cloudmechanic_1.6.0_linux_arm64.tar.gz"
-      sha256 "d552ffa33e744ae8d26c050f875cc9ed9b1d777e3dd6f35e0a13a4720b1ab2b7"
+      url "https://github.com/cloudmechanic-cli/cloudmechanic/releases/download/v1.6.1/cloudmechanic_1.6.1_linux_arm64.tar.gz"
+      sha256 "15e3051260d13c278715dba325c5e577fb792dd4d120c4036ded905b661c9efa"
     end
     on_intel do
-      url "https://github.com/cloudmechanic-cli/cloudmechanic/releases/download/v1.6.0/cloudmechanic_1.6.0_linux_amd64.tar.gz"
-      sha256 "e2a417e9f47d8fc6e3fef2fae2a0d2f50463c7725b10becf9e11af25a0fe88bc"
+      url "https://github.com/cloudmechanic-cli/cloudmechanic/releases/download/v1.6.1/cloudmechanic_1.6.1_linux_amd64.tar.gz"
+      sha256 "7561187d97e1391458cde418c12b608c671cdbaf0fa556b42dd0e5aa23b79405"
     end
   end
 
